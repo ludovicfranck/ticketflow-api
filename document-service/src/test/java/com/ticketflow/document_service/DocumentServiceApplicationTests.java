4cbd1aadc8fe5f4eb30d9b27019b909d110889b3
Franck ludovic <franckludovic351@gmail.com>
2026-04-09 14:55:20 +0100

    setup-du-projet

---
+++ document-service/src/test/java/com/ticketflow/document_service/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketflow.document_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
