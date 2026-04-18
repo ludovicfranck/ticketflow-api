14e4d5cf772c0fb2b9ea3c843a9331ac82b67597
Franck ludovic <franckludovic351@gmail.com>
2026-04-18 07:31:12 +0100

    update on pom.xml

--- api-gateway/src/main/java/com/ticketflow/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ticketflow/api_gateway/ApiGatewayApplication.java
@@ -1,11 +1,34 @@
 package com.ticketflow.api_gateway;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(
+        title = "Api Gateway of Ticketflow Api",
+        version = "1.1",
+        description = "TicketFlow est un mini-système de helpdesk permettant à des utilisateurs de créer des tickets de support, de recevoir des notifications automatiques et de joindre des documents ",
+        contact = @Contact(
+                name = "Franck Ludovic",
+                email = "franckludovic351@gmail.com",
+                url = "https://github.com/ludovicfranck/ticketflow-api.git"
+        ),
+        license = @License(
+                name = "Secure API with Spring Boot | Spring Cloud | Spring Security | Keycloak | ...",
+                url = "https://github.com/ludovicfranck/ticketflow-api.git"
+        )
+),
+        externalDocs = @ExternalDocumentation(
+                description = "Documentation of ticketflow API",
+                url = "https://github.com/ludovicfranck/ticketflow-api.git"
+        ))
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
